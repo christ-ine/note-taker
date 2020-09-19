67b99748c1e147a822cf772f37b5e4a2de9b2449
Christine Ross <christinejoyross@gmail.com>
2020-09-19 12:29:09 -0700

    post and delete

--- routes/APIroutes.js
+++ routes/APIroutes.js
@@ -1,5 +1,10 @@
 
 var notesData = require("../db/db.json");
+const fs = require('fs');
+const util = require('util');
+const { v4: uuidv4 } = require('uuid');
+const thenableWriteFile = util.promisify(fs.writeFile);
+
 
 module.exports = function(app){
 
@@ -10,14 +15,57 @@ module.exports = function(app){
     app.post("/api/notes", function(req, res) {
     
         //data sent to server is stored in req.body
-        var newNote = (req.body);
+        var noteId = uuidv4();
+        var newNote = {
+            id: noteId,
+            title: req.body.title,
+            text: req.body.text,
+        }
     
         console.log(newNote);
     
-        notesData.push(newNote);
+        
+
+        fs.readFile("./db/db.json", "utf8", (err, data) => {
+
+            if (err) throw err;
+
+            const allNotes = JSON.parse(data);
+
+            allNotes.push(newNote);
+
+            fs.writeFile("./db/db.json", JSON.stringify(allNotes), err => {
+                if (err) throw (err);
+                res.send(notesData)
+                console.log("Note successfully saved!")
+                
+            })
+
+        })
+
+        
     
-        res.json(newNote);
-    
+    })
+
+    app.delete("/api/notes/:id", function(req, res){
+        var noteDelete = req.params.id;
+
+        fs.readFile("./db/db.json", (err, data) => {
+            if (err) throw err;
+
+            const allNotes = JSON.parse(data);
+
+            const newNotebook = allNotes.filter(note => note.id =! noteDelete);
+
+            fs.writeFile("./db/db.json", JSON.stringify(newNotebook), err => {
+                if (err) throw (err);
+                res.send(notesData);
+                console.log("Note successfully deleted!")
+
+
+            })
+        })
+
     })
 
 }
